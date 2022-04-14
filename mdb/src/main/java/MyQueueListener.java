0a1d8a3b3bd86a0b07466c7f2104e53a9ab26b76
Sean Xiang <sijiex@andrew.cmu.edu>
2022-04-14 16:18:38 -0400

    lab10 in class checkpoint

--- mdb/src/main/java/MyQueueListener.java
+++ mdb/src/main/java/MyQueueListener.java
@@ -1,6 +1,16 @@
+import jakarta.annotation.Resource;
 import jakarta.ejb.ActivationConfigProperty;
 import jakarta.ejb.MessageDriven;
-import jakarta.jms.*;
+
+import jakarta.jms.Connection;
+import jakarta.jms.ConnectionFactory;
+import jakarta.jms.JMSException;
+import jakarta.jms.Message;
+import jakarta.jms.MessageListener;
+import jakarta.jms.MessageProducer;
+import jakarta.jms.Queue;
+import jakarta.jms.Session;
+import jakarta.jms.TextMessage;
 
 /*
  * MyQueueListener is a Message Listener that will listen to jms/myQueue.  Whenever a message
@@ -14,6 +24,13 @@ import jakarta.jms.*;
 })
 public class MyQueueListener implements MessageListener {
 
+    // Lookup the ConnectionFactory using resource injection and assign to cf
+    @Resource(mappedName = "jms/myConnectionFactory")
+    private ConnectionFactory cf;
+    // lookup the Queue using resource injection and assign to q
+    @Resource(mappedName = "jms/myQueue2")
+    private Queue q;
+
     /*
      * When a message is available in jms/myQueue, onMessage is called.
      */
@@ -24,6 +41,7 @@ public class MyQueueListener implements MessageListener {
                 TextMessage tm = (TextMessage) message;
                 String tmt = tm.getText();
                 System.out.println("MyQueueListener received: " + tmt);
+                writeToListner2(tmt);
             } else {
                 System.out.println("I don't handle messages of this type");
             }
@@ -31,6 +49,44 @@ public class MyQueueListener implements MessageListener {
             System.out.println("JMS Exception thrown" + e);
         } catch (Throwable e) {
             System.out.println("Throwable thrown" + e);
+        }
+    }
+
+
+    public void writeToListner2(String message) {
+        // Get the value of the simpleTextMessage parameter in the GET request
+        String val = message;
+
+        try {
+            // With the ConnectionFactory, establish a Connection, and then a Session on that Connection
+            Connection con = cf.createConnection();
+            Session session = con.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            con.start(); // Always remember to start the connection
+
+            /*
+             * You send and receive messages to/from the queue via a session. We
+             * want to send, making us a MessageProducer. Therefore, create a
+             * MessageProducer for the session
+             */
+            MessageProducer writer = session.createProducer(q);
+
+            /*
+             * The message can be text, a byte stream, a Java object, or a
+             * attribute/value Map We want to send a text message. BTW, a text
+             * message can be a string, or it can be a JSON, XML, or SOAP object.
+             */
+            TextMessage msg = session.createTextMessage();
+            msg.setText(val);
+
+            // Send the message to the destination Queue
+            writer.send(msg);
+
+            System.out.println("Sending message to QueueListener2");
+
+            // Close the connection
+            con.close();
+        } catch (Exception e) {
+            System.out.println("Servlet through exception " + e);
         }
     }
 }
